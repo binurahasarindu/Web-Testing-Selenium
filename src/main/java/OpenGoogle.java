ebef4f394df158b71d137c59de8b3fbed4d9e1cc
binurahasarindu <bhasarindu@gmail.com>
2024-09-07 11:27:54 +0530

    Link tested

---
+++ src/main/java/OpenGoogle.java
@@ -0,0 +1,16 @@
+//import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenGoogle {
+
+    //Open Google
+    //Go to Google home page
+
+    public static void main(String[] args){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+       // driver.quit();
+    }
+}
